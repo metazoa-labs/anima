e6f673ad048165c464c8f754931d8a2a0be1a94d
trinityplusone <102698957+trinityplusone@users.noreply.github.com>
2022-04-08 18:06:04 -0400

    patch

--- src/components/wallet/AccountCard.svelte
+++ src/components/wallet/AccountCard.svelte
@@ -10,9 +10,9 @@ import { Link } from "svelte-navigator";
 </script>
 
 <main>
-  <div class="uk-margin uk-card uk-card-default">
+  <div class="uk-margin uk-card uk-card-default uk-light" style="background-color:rgba(255, 255, 255, 0.06)">
     <div class="uk-card-header">
-      <h5 class="uk-card-title uk-text-uppercase uk-text-truncate">
+      <h5 class="uk-card-title uk-text-muted uk-text-uppercase uk-text-truncate">
         {account}
       </h5>
     </div>
